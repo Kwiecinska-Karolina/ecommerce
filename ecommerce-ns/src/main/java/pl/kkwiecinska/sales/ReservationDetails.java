f95360c857b05774137714a0c4f365680f1f3a76
Kwiecinska-Karolina <kwiecinska.karolina143@gmail.com>
2023-06-20 12:24:55 +0200

    update

---
+++ ecommerce-ns/src/main/java/pl/kkwiecinska/sales/ReservationDetails.java
@@ -0,0 +1,11 @@
+package pl.kkwiecinska.sales;
+
+public class ReservationDetails {
+    public ReservationDetails(String redirectUri) {
+
+    }
+
+    public Object getThankYouMessage() {
+        return null;
+    }
+}
--- ecommerce-ns/src/main/java/pl/kkwiecinska/sales/Sales.java
+++ ecommerce-ns/src/main/java/pl/kkwiecinska/sales/Sales.java
@@ -1,6 +1,12 @@
 package pl.kkwiecinska.sales;
 
+import pl.kkwiecinska.payu.OrderCreateRequest;
+import pl.kkwiecinska.payu.OrderCreateResponse;
+import pl.kkwiecinska.payu.Payu;
+
 public class Sales {
+    Payu payu;
+
     public void addToCart(String customerId, String productId) {
 
     }
@@ -10,7 +16,13 @@ public class Sales {
 
     }
 
-    public void acceptOffer(String customerId) {
+    public ReservationDetails acceptOffer(String customerId, AcceptOfferRequest acceptOffer) {
+        Offer toBeCheckouted = this.getCurrentOffer(customerId);
 
+        OrderCreateRequest orderCreateRequest = new OrderCreateRequest();
+        //
+        OrderCreateResponse resp = payu.handle(orderCreateRequest);
+
+        return new ReservationDetails(resp.getRedirectUri());
     }
 }
